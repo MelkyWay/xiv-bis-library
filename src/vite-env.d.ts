fe097db6eaf015975ebf2537d992ac1bbe69d1e0
MelkyWay <MelkyWay@users.noreply.github.com>
2026-04-01 09:53:11 +0200

    chore: inject deploy timestamp env in pages build

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,3 +3,11 @@ declare module "*.vue" {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_DEPLOYED_AT?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
